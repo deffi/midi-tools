f0141e50a1880f93a2b29dc768a7803257028a34
Martin Herrmann <martin@famherrmann.de>
2022-04-02 20:31:46 +0200

    Allow variable-length messages

--- src/midi_tools/socket_midi_bridge.py
+++ src/midi_tools/socket_midi_bridge.py
@@ -12,14 +12,45 @@ class SocketMidiBridge:
 
     def _handle_midi(self, event, _):
         message, _ = event
-        self._socket.sendall(bytes(message))
+        self._socket.sendall(len(message).to_bytes(4, "big") + bytes(message))
+
+    def _receive_length(self) -> Optional[int]:
+        data = self._socket.recv(4)
+
+        if not data:
+            return None
+
+        assert len(data) == 4
+        return int.from_bytes(data, "big")
+
+    def _receive_data(self, length) -> Optional[bytes]:
+        buffer = bytes()
+
+        while True:
+            data = self._socket.recv(length - len(buffer))
+            if not data:
+                return None
+
+            buffer += data
+            if len(buffer) == length:
+                return buffer
 
     def run(self):
         if self._midi_in:
             self._midi_in.set_callback(self._handle_midi)
 
+        buffer = bytes()
+
         try:
-            while data := self._socket.recv(3):
+            while True:
+                length = self._receive_length()
+                if length is None:
+                    break
+
+                data = self._receive_data(length)
+                if data is None:
+                    break
+
                 if self._midi_out:
                     self._midi_out.send_message(list(data))
         except ConnectionResetError:
